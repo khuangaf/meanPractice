c60fbd63b5510990756573b002ee816f9cfe17a7
steeveHuang <huangkh19951228@gmail.com>
2016-12-31 21:58:45 +0800

    first commit

---
+++ public/app.js
@@ -0,0 +1,8 @@
+var mainApplicationModuleName = 'mean';
+
+var application = angular.module(mainApplicationModuleName, []);
+
+// angular.element(document)ready(function() {
+// 	angular.bootstrap(document,
+// 		[mainApplicationModuleName]); 
+// });
---
+++ public/controllers/MainController.js
@@ -0,0 +1,4 @@
+application.controller('MainController', ['$scope',function($scope){
+	$scope.abc = "abc";
+	$scope.title = "I am awesome"
+	}]);
