cc41b26211991970378a787ae93060226b76bfaf
khuangaf <huangkh19951228@gmail.com>
2017-01-28 19:33:17 +0800

    Ff

--- app.js
+++ app.js
@@ -25,11 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.use('/webhook',function(err, req, res, next) {
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  next(1400954814);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  
 });
 
 // error handler
